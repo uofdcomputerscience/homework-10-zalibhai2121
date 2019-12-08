e142e0466ec94f3a7431d8dfba0815827405b842
Russell Mirabelli <russell@mirabellifamily.org>
2019-12-08 10:47:42 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# uofdmobileapps-homework-10
+Homework 10 starter for mobile apps
---
+++ ResultHomework.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+// Implement an Error type. Make sure it has at least two values.
+
+
+// Implement a function that returns a Result of string or your error type
+
+
+// Call your function in a way that will return an error result, and handle that error.
+
+
+// Call your function in a way that will return a success result, and handle the value.
